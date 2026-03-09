e1d3adbfbfc67cfaa77e858b96ad8bd4759116ae
Robbin Ploeger <robbplo@gmail.com>
2026-03-09 06:06:38 +0100

    Remove tests

--- src/parsing/arguments.py
+++
@@ -1,8 +0,0 @@
-import re
-
-
-def get_floats(s: str) -> list[float]:
-    # re.findall(r"[-+]?\d*\.\d+|\d+", s)
-    matches = re.findall(r"[-+]?(?:\d*\.\d+|\d+)", s)
-    print(matches)
-    return [float(x) for x in matches]
